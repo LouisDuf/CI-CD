497677937fa7b55ae8b8de372ed4c06d2fb17e3f
Louis DUFOUR <louis.dufour@etu.uca.fr>
2023-10-05 10:47:26 +0200

    Mise à jour de 'Sources/OpenLibraryWrapper/Program.cs'

--- Sources/OpenLibraryWrapper/Program.cs
+++ Sources/OpenLibraryWrapper/Program.cs
@@ -9,13 +9,49 @@ using StubbedDTO;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddSingleton<IDtoManager, Stub>();
+builder.Services.AddSingleton<IDtoManager,OpenLibClientAPI>();
 builder.Services.AddControllers();
+
+var dtoManager = Environment.GetEnvironmentVariable("OPEN_LIBRARY");
+var dbDataBase = Environment.GetEnvironmentVariable("MARIADB_DATABASE");
+var dbUser = Environment.GetEnvironmentVariable("MARIADB_USER");
+var dbPassword = Environment.GetEnvironmentVariable("MARIADB_PASSWORD");
+var dbServer = Environment.GetEnvironmentVariable("MARIADB_SERVER");
+var dbConnection = $"server={dbServer};port=3306;user={dbUser};password={dbPassword};database={dbDatabase}";
+
+switch (dtoManager)
+{
+    case "Stub":
+        builder.Services.AddSingleton<IDtoManager, Stub>();
+        break;
+    case "Api":
+        builder.Services.AddSingleton<IDtoManager, OpenLibClientAPI>();
+        break;
+    case "ApiBdd":
+        builder.Services.AddSingleton<IDtoManager, MyLibraryMgr>();
+        break;
+    case "Bdd":
+        builder.Services.AddSingleton<IDtoManager, MyLibraryMgr>(provider => new MyLibraryMgr(dbConnection));
+        break;
+    default:
+        builder.Services.AddSingleton<IDtoManager, Stub>();
+        break;
+}
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
+}
 
 app.UseHttpsRedirection();
 
